18659b555f0f227587e2de82e75a41708a0866d8
vajbharathvaj <97393645+vajbharathvaj@users.noreply.github.com>
2024-09-15 00:44:43 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,9 +2,11 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 // the env variable should start with vite in this application
-const api=axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
 
 // authorization should start with bearer and a space 
